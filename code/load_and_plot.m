b8d834f67a1e655cc020a34b27d7aa6255c039dd
Paul Chesnais <paul.chesnais@gmail.com>
2015-12-02 19:45:35 -0500

    Load and save

--- code/load_and_plot.m
+++ code/load_and_plot.m
@@ -3,7 +3,7 @@ function load_and_plot(ds)
   max_angles(max_angles == Inf) = NaN;
   max_angles = atand(max_angles);
   low = max(min(max_angles(:)), -20);
-  high = min(max(max_angles(:), 20), 20);
+  high = min(max(max_angles(:)), 20);
   figure
   imagesc(max_angles, [ low high ]);
   colormap([ 0 0 0; jet ]);
