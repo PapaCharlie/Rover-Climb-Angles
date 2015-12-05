901d450f49b85f6beca0cef22af830515867fd17
Paul Chesnais <paul.chesnais@gmail.com>
2015-12-04 20:22:17 -0500

    Gozerp indeed

--- src/queue.go
+++ src/queue.go
@@ -8,36 +8,36 @@ import (
 
 // An Item is something we manage in a priority queue.
 type Item struct {
-  value    string // The value of the item; arbitrary.
-  priority int    // The priority of the item in the queue.
-  // The index is needed by update and is maintained by the heap.Interface methods.
-  index int // The index of the item in the heap.
+  x         int
+  y         int
+  angle     float32
+  index     int
 }
 
-// A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue []*Item
+// A Frontier implements heap.Interface and holds Items.
+type Frontier []*Item
 
-func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq Frontier) Len() int { return len(pq) }
 
-func (pq PriorityQueue) Less(i, j int) bool {
-  // We want Pop to give us the highest, not lowest, priority so we use greater than here.
-  return pq[i].priority > pq[j].priority
+func (pq Frontier) Less(i, j int) bool {
+  // We want Pop to give us the highest, not lowest, angle so we use greater than here.
+  return pq[i].angle < pq[j].angle
 }
 
-func (pq PriorityQueue) Swap(i, j int) {
+func (pq Frontier) Swap(i, j int) {
   pq[i], pq[j] = pq[j], pq[i]
   pq[i].index = i
   pq[j].index = j
 }
 
-func (pq *PriorityQueue) Push(x interface{}) {
+func (pq *Frontier) Push(x interface{}) {
   n := len(*pq)
   item := x.(*Item)
   item.index = n
   *pq = append(*pq, item)
 }
 
-func (pq *PriorityQueue) Pop() interface{} {
+func (pq *Frontier) Pop() interface{} {
   old := *pq
   n := len(old)
   item := old[n-1]
@@ -46,46 +46,28 @@ func (pq *PriorityQueue) Pop() interface{} {
   return item
 }
 
-// update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+// update modifies the angle and value of an Item in the queue.
+func (pq *Frontier) update(item *Item, angle float32, angle float32) {
   item.value = value
-  item.priority = priority
+  item.angle = angle
   heap.Fix(pq, item.index)
 }
 
-// This example creates a PriorityQueue with some items, adds and manipulates an item,
-// and then removes the items in priority order.
+// This example creates a Frontier with some items, adds and manipulates an item,
+// and then removes the items in angle order.
 func main() {
-  // Some items and their priorities.
-  items := map[string]int{
-    "banana": 3, "apple": 2, "pear": 4,
-  }
-
-  // Create a priority queue, put the items in it, and
-  // establish the priority queue (heap) invariants.
-  pq := make(PriorityQueue, len(items))
-  i := 0
-  for value, priority := range items {
-    pq[i] = &Item{
-      value:    value,
-      priority: priority,
-      index:    i,
-    }
-    i++
-  }
+  // Create a angle queue, put the items in it, and
+  // establish the angle queue (heap) invariants.
+  pq := make(Frontier, 4)
+  pq[0] = Item{1,0,0.1,0}
+  pq[1] = Item{0,1,0.2,1}
+  pq[2] = Item{-1,0,1,2}
+  pq[3] = Item{0,-1,3,3}
   heap.Init(&pq)
 
-  // Insert a new item and then modify its priority.
-  item := &Item{
-    value:    "orange",
-    priority: 1,
-  }
-  heap.Push(&pq, item)
-  pq.update(item, item.value, 5)
-
-  // Take the items out; they arrive in decreasing priority order.
+  // Take the items out; they arrive in decreasing angle order.
   for pq.Len() > 0 {
     item := heap.Pop(&pq).(*Item)
-    fmt.Printf("%.2d:%s ", item.priority, item.value)
+    fmt.Printf("%2.f:(%d,%d) ", item.angle, item.x, item.y)
   }
 }
