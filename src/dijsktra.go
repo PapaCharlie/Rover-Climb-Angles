6939c5c9e16665ca214397f6a6bf8fa38f1e124d
Paul Chesnais <paul.chesnais@gmail.com>
2015-12-05 23:59:35 -0500

    Just a safety net

--- src/dijsktra.go
+++ src/dijsktra.go
@@ -38,19 +38,17 @@ func dijkstra(shape [2]int, dtmptr, max_anglesptr *[][]float64, startpos Positio
 	avg_diff := 0.0
 	for count < size && fr.Len() > 0 {
 		node := fr.PopEl()
+		count++
 		for _, n := range neighbors {
 			neighbor := node.Add(n)
 			if fits(neighbor) {
-				count++
 				diff := dtm[neighbor.x][neighbor.y] - dtm[node.x][node.y]
 				avg_diff = (avg_diff + diff) / 2.0
 				alt := math.Max(math.Abs(node.HeightDiff), math.Abs(diff))
 				if alt < math.Abs(max_angles[neighbor.x][neighbor.y]) {
 					if math.Abs(node.HeightDiff) < math.Abs(diff) {
-						max_angles[neighbor.x][neighbor.y] = diff
 						fr.PushEl(&Element{Position: neighbor, HeightDiff: diff})
 					} else {
-						max_angles[neighbor.x][neighbor.y] = node.HeightDiff
 						fr.PushEl(&Element{Position: neighbor, HeightDiff: node.HeightDiff})
 					}
 				}
