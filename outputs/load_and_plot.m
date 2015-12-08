cf6ce1ffac78554e17991aa0943031f5620cc31a
Paul Chesnais <paul.chesnais@gmail.com>
2015-12-07 21:44:40 -0500

    Makefile fix

--- outputs/load_and_plot.m
+++ outputs/load_and_plot.m
@@ -2,7 +2,7 @@ function [max_angles] = load_and_plot(ds)
   % Assuming ds argument is of the type DTEEC_xxxxxx_xxxx_yyyyyy_yyyy_yyy
   % Then ESP name will ESP_xxxxxx_xxxx
   esp = strsplit(ds, '_');
-  esp = strcat('ESP_', esp(2), '_', esp(3));
+  esp = strcat('ESP_', esp(2), '_', esp(3))
   ds = strrep(strrep(ds, '.fits',''), 'data/', '')
   site = LandingSite(ds);
   data_size = [site.label.image.lines, site.label.image.linesamples];
@@ -20,6 +20,7 @@ function [max_angles] = load_and_plot(ds)
   h = colorbar;
   ylabel(h, 'Angle required to access');
   xlim([0 size(max_angles,2)]);
+  disp(strcat('../figures/maps/', esp, '/', ds, '-traversability_map.pdf'));
   saveas(fig, strcat('../figures/maps/', esp, '/', ds, '-traversability_map.pdf'));
 
   fig = figure;
